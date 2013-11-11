5c6080a1aaab12f71a728950a541fb9a58d78c50
Sara Alspaugh <saraalspaugh@gmail.com>
2013-11-11 14:13:36 -0800

    Rendering kind of works now.

--- aptremake/static/js/aptremake.js
+++ aptremake/static/js/aptremake.js
@@ -1,34 +1,41 @@
 function render(design) {
+    
+    console.log(design)
 
     // Is the following really necessary?
     // It appears at the top of most d3 examples.
     var margin = {top: 10, right: 30, bottom: 30, left: 30},
         width = 400 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
-        
+      
     svg = d3.select("#presentation")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
-      .append("g")
+       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        
-    svg.selectAll(".dot")
+
+    points = svg.selectAll(".hi")
         .data(design.data)
         .enter().append("circle")
-        .attr("class", "dot")
-        .attr("r", 3.5)
+        .style("fill", "black")
+        .attr("r", 3)
+        .attr("mark", function(d) { return d.mark; });
+        //.attr("cx", function(d) { return Math.floor(Math.random() * 100); })
+        //.attr("cy", function(d) { return Math.floor(Math.random() * 100); });
 
     if (design.haxis) {
     
         var x = d3.scale.linear()
             .range([0, width]);
 
-        x.domain(d3.extent(data, function(d) { return d.hpos; })).nice();
+
+        x.domain(d3.extent(design.data, function(d) { return d.hpos; })).nice();
         
         var xAxis = d3.svg.axis()
             .scale(x)
-            .orient("bottom");
-       
+            .orient("bottom")
+            .ticks(5); // FIXME: Maybe this shouldn't be hard-coded.
+     
         svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
@@ -40,22 +47,21 @@ function render(design) {
             .style("text-anchor", "end")
             .text(design.hlabel);
 
-        svg.selectAll(".dot")
-            .data(design.data)
-            .enter().append("circle")
-            .attr("cx", function(d) { return x(d.x); })
+        points.attr("cx", function(d) { return x(d.hpos); });
+
     }
     
     if (design.vaxis) {
     
         var y = d3.scale.linear()
-            .range([0, height]);
+            .range([height, 0]);
         
-        y.domain(d3.extent(data, function(d) { return d.vpos; })).nice();
+        y.domain(d3.extent(design.data, function(d) { return d.vpos; })).nice();
 
-        var yAyis = d3.svg.axis()
+        var yAxis = d3.svg.axis()
             .scale(y)
-            .orient("left");
+            .orient("left")
+            .ticks(5); // FIXME: Maybe this shouldn't be hard-coded.
        
         svg.append("g")
             .attr("class", "y axis")
@@ -68,19 +74,16 @@ function render(design) {
             .style("text-anchor", "end")
             .text(design.vlabel)
         
-        svg.selectall(".dot")
-            .data(design.data)
-            .enter().append("circle")
-            .attr("cy", function(d) { return y(d.y); })
+        points.attr("cy", function(d) { return y(d.vpos); });
 
     } 
     
     if (design.color) {
 
-        svg.selectall(".dot")
-            .data(design.data)
-            .enter().append("circle")
-            .style("fill", function(d) { return color(d.color); });
+        var color = d3.scale.category10();
+
+        points.style("fill", function(d) { return color(d.color); });
+
 
         //var legend = svg.selectAll(".legend")
         //    .data(color.domain())
