6f11e83e5cb1f9c34feed2bf988266151c4bb6d9
Sara Alspaugh <saraalspaugh@gmail.com>
2013-11-11 14:12:36 -0800

    Added arity property.

--- aptremake/data.py
+++ aptremake/data.py
@@ -1,4 +1,5 @@
 
+import json
 
 class Type(object):
 
@@ -15,7 +16,8 @@ class Relation(object):
 class Set(Relation):
     
     def __init__(self, name=""):
-        self.data = None
+        self.tuples = None
+        self.arity = None
         self.type = None
         self.domain = None
         Relation.__init__(self, name=name)
@@ -28,6 +30,7 @@ class FunctionalDependency(Relation):
     def __init__(self, name=""):
         self.domain = None
         self.range = None
+        self.arity = None
         self.tuples = None
         Relation.__init__(self, name=name)
 
@@ -39,11 +42,32 @@ class CartesianProduct(Relation):
     def __init__(self, name=""):
         self.independent = None
         self.dependent = None
+        self.arity = None
         self.tuples = None
         Relation.__init__(self, name=name)
 
     def __repr__(self):
         return " ".join(["CartesianProduct:", self.name, "(sets:", str(self.sets) + ")"])
 
-def read_data(spec):
-    pass
+classes = {
+    "Set": Set, 
+    "FunctionalDependency": FunctionalDependency,
+    "CartesianProduct": CartesianProduct
+}
+
+def read_data(specfilename):
+    data = {}
+    with open(specfilename) as specfile:
+        spec = json.load(specfile)
+        for s in spec:
+            d = classes[s["class"]](name=s["name"])
+            d.type = s.get("type", None)
+            d.domain = s.get("domain", None)
+            d.tuples = s["data"]
+            d.arity = s["arity"]
+            data[s["name"]] = d
+        for s in spec:
+            if s["class"] == "FunctionalDependency":
+                data[s["name"]].domain = data[s["domain"]]
+                data[s["name"]].range = data[s["range"]]
+    return data
